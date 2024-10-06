cad952d36832189bcf4c363aa2dabd9daca441e1
Joshua Lee <josh@192.168.20.45>
2024-10-06 14:34:54 +1030

    this is confusing

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -4,16 +4,21 @@
 using namespace std;
 
 #include "Pokemon.h"
+#include "Type.h"
 
 Pokemon::Pokemon() {}
-Pokemon::Pokemon(string n, int num, int lv) : name(n), number(num), level(lv) {}
-int Pokemon::levelUp() {}
+Pokemon::Pokemon(string n, Type t, int h, int lv)
+    : name(n), type(t), health(h), level(lv) {}
 int Pokemon::takeDamage() {}
 int Pokemon::heal() {}
+int Pokemon::levelUp() {}
 
 string Pokemon::get_name() { return name; }
 void Pokemon::set_name(string name) { this->name = name; }
-int Pokemon::get_number() { return number; }
-void Pokemon::set_number(int number) { this->number = number; }
+int Pokemon::get_health() { return health; }
+void Pokemon::set_health(int health) { this->health = health; }
 int Pokemon::get_level() { return level; }
 void Pokemon::set_level(int level) { this->level = level; }
+
+int Pokemon::get_number() { return number; }
+void Pokemon::set_number(int number) { this->number = number; }
--- Pokemon.h
+++ Pokemon.h
@@ -4,27 +4,37 @@
 #include <iostream>
 #include <string>
 
+#include "Type.h"
+
 using namespace std;
 
 class Pokemon {
  protected:
   string name;
-  int number;
+  Type type;
+  int health;
   int level;
+
+  // for list:
+  int number;
 
  public:
   Pokemon();
-  Pokemon(string n, int num, int lv);
-  int levelUp();
+  Pokemon(string n, Type t, int h, int lv);
   int takeDamage();
   int heal();
+  int levelUp();
 
   string get_name();
   void set_name(string name);
-  int get_number();
-  void set_number(int number);
+  int get_health();
+  void set_health(int health);
   int get_level();
   void set_level(int level);
+
+  // for list:
+  int get_number();
+  void set_number(int number);
 };
 
 #endif
