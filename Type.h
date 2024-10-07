1c14e531c269bc478201eae3b35e02303dc4151c
Joshua Lee <josh@192.168.20.45>
2024-10-07 16:37:55 +1030

    types are on its way

--- Type.h
+++ Type.h
@@ -7,32 +7,26 @@
 using namespace std;
 
 // Enumeration for Pokémon types
-enum TypeName { FIRE, WATER, GRASS, ELECTRIC, NUM_TYPES };
-
-// Function to convert TypeName enum to string
-std::string typeToString(TypeName type) {
-  switch (type) {
-    case FIRE:
-      return "Fire";
-    case WATER:
-      return "Water";
-    case GRASS:
-      return "Grass";
-    case ELECTRIC:
-      return "Electric";
-    default:
-      return "Unknown";
-  }
-}
-
-// 2D array for type effectiveness
-// Rows represent attack type, columns represent defense type
-float typeEffectiveness[NUM_TYPES][NUM_TYPES] = {
-    // FIRE, WATER, GRASS, ELECTRIC
-    {1.0, 0.5, 2.0, 1.0},  // Fire vs [Fire, Water, Grass, Electric]
-    {2.0, 1.0, 0.5, 1.0},  // Water vs [Fire, Water, Grass, Electric]
-    {0.5, 2.0, 1.0, 1.0},  // Grass vs [Fire, Water, Grass, Electric]
-    {1.0, 2.0, 0.5, 1.0}   // Electric vs [Fire, Water, Grass, Electric]
+enum TypeName {
+  NORMAL,
+  FIRE,
+  WATER,
+  ELECTRIC,
+  GRASS,
+  ICE,
+  FIGHTING,
+  POISON,
+  GROUND,
+  FLYING,
+  PSYCHIC,
+  BUG,
+  ROCK,
+  GHOST,
+  DRAGON,
+  DARK,
+  STEEL,
+  FAIRY,
+  NUM_TYPES
 };
 
 class Type {
