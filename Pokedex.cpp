139816b9c425a62b96ec9c2927ec4570df9f29b3
Joshua Lee <josh@Joshuas-MacBook-Air-2.local>
2024-10-11 17:38:55 +1030

    using jay code

---
+++ Pokedex.cpp
@@ -0,0 +1,48 @@
+#include "Pokedex.h"
+
+#include <iostream>
+
+using namespace std;
+
+Pokedex::Pokedex() {
+  pokedex_capacity = 0;
+  pokedex_array = new Pokemon[party_capacity];
+}
+
+Pokedex::Pokedex(int capacity) {
+  pokedex_capacity = capacity;
+  pokedex_array = new Pokemon[party_capacity];
+}
+
+void Pokedex::display_pokedex() {
+  if (party_array == nullptr) {
+    cout << "Pokedex is empty." << endl;
+    return;
+  }
+  cout << "This is the Pokedex, which shows the type, health and moveset of "
+          "every Pokemon available:"
+       << endl;
+  int size = party_capacity;
+  for (int i = 0; i < size; i++) {
+    cout << "Pokemon " << i + 1 << ":" << endl;
+    cout << " Species: " << party_array[i].get_species() << " | ";
+    cout << " Type: " << party_array[i].get_type() << " | ";
+    cout << " Health: " << party_array[i].get_health() << " | ";
+    cout << " Moveset: " << party_array[i].get_moveset() << endl;
+    cout << "\n";
+  }
+}
+void Pokedex::rem_pokemon_from_pokedex(Pokemon pokemon) {
+  rem_pokemon_from_party(pokemon);
+}
+
+void Pokedex::add_pokemon_to_pokedex(Pokemon pokemon) {
+  if (party_size < party_capacity) {
+    party_array[party_size] = pokemon;
+    party_size++;
+  } else {
+    cout << "Pokemon party is full!" << endl;
+  }
+}
+
+Pokedex::~Pokedex() { delete[] party_array; }
---
+++ Pokedex.h
@@ -0,0 +1,23 @@
+#ifndef POKEDEX_H
+#define POKEDEX_H
+
+#include "Pokemon.h"
+#include "PokemonParty.h"
+
+using namespace std;
+
+class Pokedex : public Pokemon, PokemonParty {
+ public:
+  Pokemon* pokedex_array;
+  int pokedex_capacity;
+
+  Pokedex();
+  Pokedex(int capacity);
+
+  void display_pokedex();
+  void add_pokemon_to_pokedex(Pokemon pokemon);
+  void rem_pokemon_from_pokedex(Pokemon pokemon);
+  ~Pokedex();
+};
+
+#endif
